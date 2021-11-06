bafccc7e8a2953c1c900bc30044076556928ee98
Rakib Hasan <rakib.cs132@gmail.com>
2021-11-06 23:40:21 +0600

    cs204 lab 4 insertion sort

---
+++ CS204/Lab 4/7_sorting/a_insertion_sort.c
@@ -0,0 +1,38 @@
+/*
+1. Iterate all the elements
+2. Compate the current value with previous
+    - if prev > current then swape
+    - else break the loop
+*/
+
+#include <stdio.h>
+
+void swape(int *n1, int *n2)
+{
+    int temp = *n1;
+    *n1 = *n2;
+    *n2 = temp;
+}
+
+int main()
+{
+    int i, j, total;
+    int arr[] = {1, 9, 2, 7, 3, 8, 6, 4, 5};
+    total = sizeof(arr) / sizeof(arr[0]);
+    for (i = 0; i < total - 1; i++)
+    {
+        for (j = i + 1; j > 0; j--)
+        {
+            if (arr[j - 1] < arr[j])
+            {
+                break;
+            }
+            swape(&arr[j - 1], &arr[j]);
+        }
+    }
+
+    for (i = 0; i < total; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+}
